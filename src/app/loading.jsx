8cafc44a76cfe09b346b7e91b982f998bee700b4
dminuslabs <dminuslabs@gmail.com>
2026-07-18 10:29:43 +0000

    fixed the header button visiblity/

---
+++ src/app/loading.jsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+  return (
+    <div id="preloader">
+      <div className="preloader">
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
+}
